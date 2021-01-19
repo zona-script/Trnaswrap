99e369dc85419c104b32df97b577c6b02913b0c6
wangye <553311122@qq.com>
2021-01-19 15:26:46 +0800

    添加交易对接口

--- src/api/api.js
+++ src/api/api.js
@@ -24,4 +24,14 @@ export function getUserTeam(params) {
   return fet('api/team', params, 'get')
 }
 
+//获取token信息
+export function getTokenInfo(params) {
+  return fet('api/coin/tokenInfo', params, 'get')
+}
+
+//获取交易对信息
+export function getPairInfo(params) {
+  return fet('api/coin/pairInfo', params, 'get')
+}
+
 export { api }
