ddbd16671665072080c69259d6c2efbd103aa44e
gu111111 <461480134@qq.com>
2021-01-24 13:45:59 +0800

    删除header.less引入

--- src/views/createApair/createApair.vue
+++ src/views/createApair/createApair.vue
@@ -223,7 +223,6 @@ please click "Confirm" when you have confirmedthe above proportion.</p>
 </script>
 <style lang="less">
   @import "@/themes/style/common.less";
-  @import "@/themes/style/header.less";
   @import "@/themes/style/button.less";
   @import "@/themes/style/pannel.less";
   @import "@/themes/style/formview.less";
