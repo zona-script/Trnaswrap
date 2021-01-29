f4934995d41f14a28bb316c0f01eaac78ce4183e
wangye <553311122@qq.com>
2021-01-29 11:10:10 +0800

    1

--- src/utils/tronwebFn.js
+++ src/utils/tronwebFn.js
@@ -54,12 +54,9 @@ const bPoolAllowance = (coinAddress, contractAddress) => { // BPool查询授权
     }
   })
 }
-// const Web3Utils = require('web3');
-// const MAX = Web3Utils.utils.toTwosComplement(-1);
 const approved = (coinAddress, contractAddress) => { // 授权
   return new Promise(function(resolve, reject) {
     try {
-      // console.log('MAX================'+MAX)
       window.tronWeb.contract().at(coinAddress).then((Contract) => {
         Contract['approve'](contractAddress, '100000000000000000000000000').send({ shouldPollResponse: true }).then((res) => {
           if (res) {
